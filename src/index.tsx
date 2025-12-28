9b1de20eeda0e9ee30d423717326dde88e773395
Mid <noobability21@gmail.com>
2025-12-28 01:56:29 -0500

    sdsdsds

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // ← This should be here, not in App.tsx
 import App from './App';
 
 const root = ReactDOM.createRoot(
