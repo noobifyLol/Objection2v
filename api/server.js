f44d4894b92816098921c53c94db488d017f7be6
Mid <noobability21@gmail.com>
2025-12-30 15:06:40 -0500

    wdasdasd

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
