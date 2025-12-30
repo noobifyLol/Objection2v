9ec2b4918c42f5d40c00cc538708246105264753
Mid <noobability21@gmail.com>
2025-12-30 12:31:51 -0500

    f

--- api/server.js
+++ api/server.js
@@ -5,7 +5,8 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+import { fileURLToPath } from "url";
+import path from "path";
 dotenv.config();
 
 /* =======================
@@ -175,7 +176,7 @@ app.get("/test-gemini", async (_req, res) => {
 ======================= */
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (_req, res) => {
+app.get("/*", (_req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
